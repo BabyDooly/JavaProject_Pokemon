497f5616f616863676804b585837c7a726a303d4
SeongJu <sp1799@naver.com>
2022-10-25 21:50:39 +0900

    Dbcon

---
+++ core/src/com/pokemon/db/DbCon.java
@@ -0,0 +1,7 @@
+package com.pokemon.db;
+
+public class DbCon {
+    static String DBURL = "jdbc:mysql://localhost:3306/pokemon?serverTimezone=UTC";
+    static String DBID = "root";
+    static String DBPASS = "root";
+}
